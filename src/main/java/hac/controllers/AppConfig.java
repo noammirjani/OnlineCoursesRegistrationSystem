a2f38c7d6c2ba71c24daa07cfde58e629335a9a0
Ariel1445 <70839342+Ariel1445@users.noreply.github.com>
2023-06-12 20:01:40 +0300

    All configuration applyed

--- src/main/java/hac/controllers/AppConfig.java
+++ src/main/java/hac/controllers/AppConfig.java
@@ -13,19 +13,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
-
 @Configuration
 public class AppConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder bCryptPasswordEncoder) {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user")
-                .password(bCryptPasswordEncoder.encode("password"))
+        manager.createUser(User.withUsername("Ariel")
+                .password(bCryptPasswordEncoder.encode("1234"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("Noam")
+                .password(bCryptPasswordEncoder.encode("1234"))
                 .roles("USER")
                 .build());
         manager.createUser(User.withUsername("admin")
-                .password(bCryptPasswordEncoder.encode("password"))
+                .password(bCryptPasswordEncoder.encode("12345"))
                 .roles("ADMIN")
                 .build());
         return manager;
@@ -41,19 +44,17 @@ public class AppConfig {
         http.cors(withDefaults())
                 .csrf(withDefaults())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("static/**", "/css/**", "/", "/register", "/courses").permitAll()
+                        .requestMatchers("static/**", "/css/**", "/403", "/errorpage","/", "/register", "/courses").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER"))
                 .formLogin((form) -> form
-                                .loginPage("/login")
+                                 .loginPage("/login")
 //                               .loginProcessingUrl("/perform_login")
-                                .defaultSuccessUrl("/", true)
-//                               .failureUrl("/login?error=true")
+//                               .defaultSuccessUrl("/", true)
+                                 .failureUrl("/login?error=true")
                                 .permitAll())
                 .logout((logout) -> logout.permitAll())
                 .exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
                 return http.build();
     }
-
-
 }
--- src/main/java/hac/controllers/MainController.java
+++ src/main/java/hac/controllers/MainController.java
@@ -1,16 +1,14 @@
 package hac.controllers;
-import org.springframework.stereotype.Controller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import java.security.Principal;
@@ -21,7 +19,6 @@ import java.security.Principal;
 public class MainController {
 
     private static Logger logger = LoggerFactory.getLogger(MainController.class);
-
     @RequestMapping("/")
     public String index() {  // add Principal principal to argument to show loged user details
         return "index";
@@ -45,6 +42,33 @@ public class MainController {
     @RequestMapping("/user")
     public String userIndex() {
         return "user/index";
+    }
+
+    @RequestMapping("/admin")
+    public String adminIndex() {
+        return "admin/index";
+    }
+
+    @RequestMapping("/403")
+    public String forbidden() {
+        return "403";
+    }
+
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception ex, Model model) {
+
+        //logger.error("Exception during execution of SpringSecurity application", ex);
+        String errorMessage = (ex != null ? ex.getMessage() : "Unknown error");
+
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
+    }
+
+    @RequestMapping("/errorpage")
+    public String error(Exception ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
     }
 
 }
